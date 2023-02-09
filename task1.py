ee19f90beb9d2c3153d50230f64665071bb7a307
MikoYurei <100051168+MikoYurei@users.noreply.github.com>
2023-02-09 08:06:13 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-L = [5, 4, 3, 2, 1]
-for i in range(0, len(L)):
-    for j in range(0, len(L)-i-1):
-        if L[j+1] < L[j]:
-            a = L[j]
-            L[j] = L[j+1]
-            L[j+1] = a
-print("L = ", L)
